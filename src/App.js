7c45da945c0024214b5d53b23ba9b9ccf26bbda0
nightmono <163682003+nightmono@users.noreply.github.com>
2024-10-26 12:38:17 +0100

    Add Item components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Item() {
+  return (
+    <div style={{border: "2px solid black", "border-radius": "4px", maxWidth: "60rem"}}>
+      <h1>Item Name</h1>
+      <p>Item description</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Wordl!</h1>
+      <Item />
     </div>
   );
 }

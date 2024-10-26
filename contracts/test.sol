331b2e726f7ce1f406cfeb8515a6e0422db5409c
Dragos.py <dragosesoalca@gmail.com>
2024-10-26 22:56:30 +0100

    Added krnl stuff

--- contracts/test.sol
+++ contracts/test.sol
@@ -1,19 +1,28 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
 
-contract Listing {
+import {KrnlRelgistered, KrnlPayload} from "./KrnlRegistered.sol";
+
+contract Listing is KrnlRegistered {
     //Properties
     uint public data;
  
-    constructor(){
+    constructor(address _tokenAuthorityPublicKey) KrnlRegistered(_tokenAuthorityPublicKey) {
         data = 0;
     }
- 
-    function setData(uint _data) public {
+
+    function setData(
+        KrnlPayload memory krnlPayload,
+        uint _data
+    )
+        external
+        onlyAuthorized(krnlPayload, abi.encodePacked(key, value))
+    {
         data = data + _data;
     }
- 
+
     function getData() public view returns (uint) {
         return data;
     }
+
 }

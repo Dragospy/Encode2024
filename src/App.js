66e78554a93cd801e8f5744da2723c1face88e65
Dragos.py <dragosesoalca@gmail.com>
2024-10-27 10:27:06 +0000

    Product listing is now done using contract

--- src/App.js
+++ src/App.js
@@ -1,92 +1,10 @@
-import './App.css';
 import React from 'react';
-import logo from './logo.svg'
 import { useState } from 'react';
 
-function Dropdown() {
-  const [selectedOption, setSelectedOption] = useState('');
-
-  const options = [
-    { value: 'art', label: 'Art' },
-    { value: 'nft', label: 'NFTs' },
-  ];
-
-  const handleChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
-
+function App() {
   return (
     <div>
-      <select value={selectedOption} onChange={handleChange}>
-        <option value="">Filter</option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-};
 
-function SearchBar() {
-  return (
-    <input className='searchBar'></input>
-  )
-}
-
-function Top() {
-  return (
-    <div className='top'>
-      <span className='largeText'>decentralise</span>
-      <SearchBar />
-      <Dropdown />
-    </div>
-  )
-}
-
-function Listing({ listing }) {
-  return (
-    <div className="listing">
-      <div className='listingImage'></div>
-      <div className="ListingName">{listing.name}</div>
-      <div className='listingPrice'><strong>{listing.price}</strong></div>
-    </div>
-  );
-}
-
-function ListingContainer({ children }) {
-  return (
-    <div className='listingContainer'>
-      {children}
-    </div>
-  );
-}
-
-function App() {
-  const generateRandomPrice = () => {
-    return (Math.random() * 10).toFixed(3);
-  };
-
-  const exampleData = [];
-
-  for (let i = 1; i < 11; i++) {
-    const name = `Example ${i}`;
-    const price = `${generateRandomPrice()} ETH`;
-    const listing = {name: name, price: price};
-    
-    exampleData.push(
-      <Listing listing={listing} />
-    );
-  }
-
-  return (
-    <div className="App">
-      <Top />
-      <p className='largeText'>Your decentralised marketplace</p>
-      <ListingContainer>
-        {exampleData}
-      </ListingContainer>
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import './css/App.css';
+/*import './css/App.css';
 import React, { useState, useEffect , useRef} from 'react';
 import logo from './svgs/logo.svg'
 import MarketPlace from "./contractJsons/MarketPlace.json";
@@ -88,4 +88,165 @@ function HomePage() {
   );
 }
 
+export default HomePage;*/
+
+import './css/App.css';
+import React, { useEffect } from 'react';
+import logo from './svgs/logo.svg'
+import { useState, useAsync} from 'react';
+import MarketPlace from "./contractJsons/MarketPlace.json";
+import Navbar from '../components/Navbar'
+import { wait } from '@testing-library/user-event/dist/utils';
+const ethers = require("ethers");
+const contractAddress = "0xC56a93C923e2105830B450c311F4061D021E4A06";
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+
+function Dropdown() {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const options = [
+    { value: 'art', label: 'Art' },
+    { value: 'nft', label: 'NFTs' },
+  ];
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <select value={selectedOption} onChange={handleChange}>
+        <option value="">Filter</option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+function SearchBar() {
+  return (
+    <input className='searchBar'></input>
+  )
+}
+
+function Top() {
+  return (
+    <div className='top'>
+      <span className='largeText'>decentralise</span>
+      <SearchBar />
+      <Dropdown />
+    </div>
+  )
+}
+
+function Listing({ listing }) {
+  return (
+    <div className="listing">
+      <div className='listingImage'></div>
+      <div className="ListingName">{listing.name}</div>
+      <div className='listingPrice'><strong>{listing.price}</strong></div>
+    </div>
+  );
+}
+
+function ListingContainer({ children }) {
+  return (
+    <div className='listingContainer'>
+      {children}
+    </div>
+  );
+}
+
+const GetMarketListings = async () => {
+  try {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const Signer = await provider.getSigner();
+
+      // Create a new instance of the Contract class
+      const Contract = new ethers.Contract(contractAddress, MarketPlace.abi, Signer);
+
+      // Call the getValue function from the contract
+      const listings = await Contract.getListings();
+      //console.log('Listings Array :', listings[1]);
+      
+      return listings
+  } catch (error) {
+    console.error("This is what happened")
+    console.log(error);
+    alert(error);
+  }
+};
+function HomePage()  {
+  const [listings, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData(){
+
+      try {
+        const listings = await GetMarketListings();
+        setData(listings);
+        setLoading(false);
+      }
+      catch (error){
+        setError(error);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  })
+
+  const generateRandomPrice = () => {
+    return (Math.random() * 10).toFixed(3);
+  };
+
+  if (loading) {
+    return <div> Loading...</div>
+  }
+
+  if (error) {
+    return <div> error.message</div>
+  }
+
+
+  const exampleData = [];
+  for (let i = 0; i < listings.length; i++){
+    if (listings[i].name != ""){
+      const name = listings[i].name;
+      const price = `${listings[i].price} ETH`;
+      const listing = {name: name, price: price};
+      
+      exampleData.push(
+        <Listing listing={listing} />
+    );
+    }
+  }
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Top />
+      <p className='largeText'>Your decentralised marketplace</p>
+        <ListingContainer>
+          {exampleData}
+        </ListingContainer>
+    </div>
+  );
+}
+
 export default HomePage;
+

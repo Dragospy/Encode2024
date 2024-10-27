78dbf1466c126265dd04b8e0d7a2b16d38bb577c
Dragos.py <dragosesoalca@gmail.com>
2024-10-27 10:31:34 +0000

    Removed some unecessary stuff

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,95 +1,3 @@
-/*import './css/App.css';
-import React, { useState, useEffect , useRef} from 'react';
-import logo from './svgs/logo.svg'
-import MarketPlace from "./contractJsons/MarketPlace.json";
-import Navbar from '../components/Navbar'
-const ethers = require("ethers");
-const contractAddress = "0xC56a93C923e2105830B450c311F4061D021E4A06";
-
-function SearchBar() {
-  return (
-    <input className='searchBar'></input>
-  )
-}
-
-function Listing({ listing }) {
-  return (
-    <div className="listing">
-      <div className="ListingName">{listing.name}</div>
-      <div className='listingPrice'>{listing.price}</div>
-    </div>
-  );
-}
-
-function ListingContainer({ children }) {
-  return (
-    <div className='listingContainer'>
-      {children}
-    </div>
-  );
-}
-
-const GetMarketListings = async () => {
-  try {
-    if (window.ethereum) {
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      const Signer = await provider.getSigner();
-
-      // Create a new instance of the Contract class
-      const Contract = new ethers.Contract(contractAddress, MarketPlace.abi, Signer);
-
-      // Call the getValue function from the contract
-      const listings = await Contract.getListings();
-      console.log('Listings Array :', listings);
-      
-      return listings
-    } else {
-      console.error(
-        'MetaMask not found. Please install MetaMask to use this application.',
-      );
-    }
-  } catch (error) {
-    console.error("This is what happened")
-    console.log(error);
-    alert(error);
-  }
-};
-
-function HomePage() {
-  const exampleListing = { image: { logo }, imageAlt: "React logo", name: "Example", price: "0.02 ETH" }
-
-  function DisplayListings(){
-    var listings = GetMarketListings();
-    const listingsToRender = () => {
-      const listItems = [];
-      for (let i = 0; i < listings.length; i++){
-        if (listings[i].name != ""){
-          console.log(listings[i])
-          listItems.push(<Listing listing = {{name: listings[i].name, price: listings[i].price}}/>);
-        }
-      }
-      return listItems;
-    }
-  
-    return listingsToRender;
-  }
-
-  const ListingsToDisplay = DisplayListings();
-
-
-  return (
-    <div>
-      <Navbar />
-      <SearchBar />
-      <ListingContainer>
-        <listingsToDisplay/>
-      </ListingContainer>
-    </div>
-  );
-}
-
-export default HomePage;*/
-
 import './css/App.css';
 import React, { useEffect } from 'react';
 import logo from './svgs/logo.svg'
@@ -223,14 +131,14 @@ function HomePage()  {
   }
 
 
-  const exampleData = [];
+  const listingData = [];
   for (let i = 0; i < listings.length; i++){
     if (listings[i].name != ""){
       const name = listings[i].name;
       const price = `${listings[i].price} ETH`;
       const listing = {name: name, price: price};
       
-      exampleData.push(
+      listingData.push(
         <Listing listing={listing} />
     );
     }
@@ -242,7 +150,7 @@ function HomePage()  {
       <Top />
       <p className='largeText'>Your decentralised marketplace</p>
         <ListingContainer>
-          {exampleData}
+          {listingData}
         </ListingContainer>
     </div>
   );
